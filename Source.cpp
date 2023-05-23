a912efe79ebe9d46bf03a8f77ccbdec2faf1ca9a
Mahalu-Ciprian-FMI <128313829+Mahalu-Ciprian-FMI@users.noreply.github.com>
2023-05-23 19:38:17 +0300

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -1,7 +1,7 @@
 #include "clase.h"
 int main()
 {
-	/*std::string current_month;
+	std::string current_month;
 	int prima;
 	int current_month_int;
 	int current_an;
@@ -39,7 +39,7 @@ int main()
 	}
 	std::cout << "Introduceti bonusul: ";
 	std::cin >> prima;
-	std::vector<Angajat*> v;
+	/*std::vector<Angajat*> v;
 	std::cout << "Introduceti numarul de angajati al firmei: ";
 	int angajare;
 	std::cin >> angajare;
@@ -145,7 +145,7 @@ int main()
 				}
 				else
 				{
-					
+
 					if (p->get_final_contract().get_an() == current_an)
 					{
 						float a = p->get_salariu() + 0.75 * prima;
@@ -189,6 +189,7 @@ int main()
 	y2 = y.name("A").pname("C").salary(5250).emp_date(Data(6, "septembrie", 2022)).ore_luc(4).fire_date(Data(19, "decembrie", 2023)).finish();
 	y3 = y.name("P").pname("L").salary(9000).emp_date(Data(18, "noiembrie", 2020)).ore_luc(4).fire_date(Data(5, "decembrie", 2021)).finish();
 	y4 = y.name("R").pname("S").salary(2300).emp_date(Data(20, "august", 2019)).ore_luc(4).fire_date(Data(31, "ianuarie", 2021)).finish();
+	//y5 = y.name("R").pname("S").salary(2300).emp_date(Data(20, "august", 2019)).ore_luc(4).fire_date(Data(31, "ianuarie", 2021)).finish();
 	connection_pool obj1;
 	try
 	{
@@ -222,5 +223,120 @@ int main()
 		data4.afisare();
 	}
 	catch (std::runtime_error& a) { std::cout << a.what() << "\n"; }
+	//
+	std::vector<Angajat*> v;
+	std::vector<Permanent> v1;
+	std::vector<Part_Time> v2;
+	v.push_back(&x1);
+	v.push_back(&y1);
+	v.push_back(&y2);
+	v.push_back(&y3);
+	v.push_back(&y4);
+	//v.push_back(&y5);
+	for (auto p = v.begin();p != v.end();p++)
+	{
+		if (typeid(**p) == typeid(Permanent))
+			v1.push_back(*dynamic_cast<Permanent*>(*p));
+		else
+			v2.push_back(*dynamic_cast<Part_Time*>(*p));
+	}
+	for (auto p = v1.begin();p != v1.end();p++)
+	{
+		try
+		{
+			if (p->get_nr_minori() < 0)
+				throw(p->get_nr_minori());
+			else
+				if (p->get_nr_minori() == 0)
+				{
+					float a = p->get_salariu() + prima;
+					p->set_salariu(a);
+				}
+				else
+				{
+					float a = p->get_salariu() + (p->get_nr_minori() * (2020 - p->get_data_angajare().get_an()) * prima / 100) + prima;
+					p->set_salariu(a);
+				}
+		}
+		catch (int nr_copii)
+		{
+			std::cout << "\nO persoana nu poate avea un numar de copii negativ, precum: " << nr_copii << "\n";
+		}
+	}
+	int b = NULL;
+	for (auto p = v2.begin();p != v2.end();p++)
+	{
+		try
+		{
+			if (p->get_final_contract().get_zi() > 31 || p->get_final_contract().get_zi() < 1)
+				throw(p->get_final_contract().get_zi());
+			else
+			{
+				if (1)
+				{
+					if (p->get_final_contract().get_luna() == "ianuarie")
+						b = 1;
+					if (p->get_final_contract().get_luna() == "februarie")
+						b = 2;
+					if (p->get_final_contract().get_luna() == "martie")
+						b = 3;
+					if (p->get_final_contract().get_luna() == "april")
+						b = 4;
+					if (p->get_final_contract().get_luna() == "mai")
+						b = 5;
+					if (p->get_final_contract().get_luna() == "iunie")
+						b = 6;
+					if (p->get_final_contract().get_luna() == "iulie")
+						b = 7;
+					if (p->get_final_contract().get_luna() == "august")
+						b = 8;
+					if (p->get_final_contract().get_luna() == "septembrie")
+						b = 9;
+					if (p->get_final_contract().get_luna() == "octombrie")
+						b = 10;
+					if (p->get_final_contract().get_luna() == "noiembrie")
+						b = 11;
+					if (p->get_final_contract().get_luna() == "decembrie")
+						b = 12;
+				}
+				if (b > current_month_int && p->get_final_contract().get_an() == current_an || p->get_final_contract().get_an() > current_an)
+				{
+					float a = p->get_salariu() + prima;
+					p->set_salariu(a);
+				}
+				else
+				{
+
+					if (p->get_final_contract().get_an() == current_an)
+					{
+						float a = p->get_salariu() + 0.75 * prima;
+						p->set_salariu(a);
+					}
+				}
+			}
+		}
+		catch (int zi)
+		{
+			std::cout << "O luna nu poate avea " << zi << " zile";
+		}
+	}
+
+	std::cout << "\n";
+	for (auto p = v1.begin();p != v1.end();p++)
+	{
+		std::cout << " permanent: ";
+		p->afisare();
+		std::cout << "\n";
+	}
+	for (auto p = v2.begin();p != v2.end();p++)
+	{
+		std::cout << "\n";
+		if (p->get_final_contract().get_an() >= 2020)
+			p->afisare();
+		else
+			//std::cout << " Numele fostului angajat este: " << p->get_nume() << " " << p->get_prenume();
+			std::cout << "\n";
+	}
+	//
 	return 0;
 }
